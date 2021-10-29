6df8a17c5b95e06e3b4743776ededcae89af0f0a
krol1999 <sasagricenko916@gmail.com>
2021-10-29 13:56:40 +0300

    generator

---
+++ objectInSimpleLanguage/generator.js
@@ -0,0 +1,44 @@
+function* strGenerator() { //* возле функции (или названия функции) обозначает, что она является генератором
+    yield 'H' //порционно выдает результат
+    yield 'e'
+    yield 'l'
+    yield 'l'
+    yield 'o'
+}
+
+const str = strGenerator()
+
+function* numberGen(n= 10) {
+    for (let i =0; i < n; i++) {
+        yield i
+
+    }
+}
+
+const num = numberGen(7)
+
+const iterator = {
+    [Symbol.iterator](n=10) {
+        let i = 0
+        return {
+            next() {
+                if (i < n) {
+                    return {value: ++i, done: false}
+                }
+                return {value: undefined, done: true}
+            }
+
+        }
+    }
+}
+
+function* iter(n = 10) {
+
+    for (let i = 0; i < n; i++) {
+        yield i
+    }
+}
+
+for (let k of iter(6)) {
+    console.log(k)
+}
