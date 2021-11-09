4875c8bb92e180124a389c17876a7de1cd6a0d0b
krol1999 <sasagricenko916@gmail.com>
2021-11-09 07:28:20 +0300

    i am alive

---
+++ practice.js
@@ -0,0 +1,14 @@
+let number = prompt('Введите число: ')
+switch (number){
+    case '0' :
+        alert('Вы введи число 0')
+        break
+    case '1' :
+        alert('Вы введи число 1')
+        break
+    case '2' :
+    case '3' :
+        alert('Вы введи число 2 или 3')
+        break
+}
+
